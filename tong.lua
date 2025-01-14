df9f88b2f202a7d927754e8f26e32f57a063eff7
Ho-spair <linyuy@163.com>
2025-01-14 23:03:49 +0800

    同步谋夏侯惇

--- tong.lua
+++ tong.lua
@@ -1329,27 +1329,34 @@ local ganglie = fk.CreateActiveSkill{
   anim_type = "offensive",
   prompt = "#mou__ganglie",
   card_num = 0,
-  target_num = 1,
+  min_target_num = 1,
   can_use = function(self, player)
     return player:usedSkillTimes(self.name, Player.HistoryPhase) == 0
   end,
   card_filter = Util.FalseFunc,
   target_filter = function(self, to_select, selected)
     return
-      #selected == 0 and
       table.contains(Self:getTableMark("mou__ganglie_enemy"), to_select) and
       not table.contains(Self:getTableMark("mou__ganglie_targeted"), to_select)
   end,
   on_use = function(self, room, effect)
     local player = room:getPlayerById(effect.from)
-    room:addTableMarkIfNeed(player, "mou__ganglie_targeted", effect.tos[1])
+    local record = player:getTableMark("mou__ganglie_targeted")
+    table.insertTableIfNeed(record, effect.tos)
+    room:setPlayerMark(player, "mou__ganglie_targeted", record)
 
-    room:damage{
-      from = player,
-      to = room:getPlayerById(effect.tos[1]),
-      damage = 2,
-      skillName = self.name,
-    }
+    for _, toId in ipairs(effect.tos) do
+      local to = room:getPlayerById(toId)
+
+      if to:isAlive() then
+        room:damage{
+          from = player,
+          to = to,
+          damage = 2,
+          skillName = self.name,
+        }
+      end
+    end
   end,
 }
 local ganglieRecord = fk.CreateTriggerSkill{
@@ -1390,8 +1397,8 @@ local ganglieRecord = fk.CreateTriggerSkill{
 }
 Fk:loadTranslationTable{
   ["mou__ganglie"] = "刚烈",
-  [":mou__ganglie"] = "每名角色限一次，出牌阶段限一次，你可以对一名对你造成过伤害的角色造成2点伤害。",
-  ["#mou__ganglie"] = "刚烈：你可对其中一名角色造成1点伤害",
+  [":mou__ganglie"] = "每名角色限一次，出牌阶段限一次，你可以对至少一名对你造成过伤害的角色造成2点伤害。",
+  ["#mou__ganglie"] = "刚烈：你可选择至少一名可选角色，各对其造成2点伤害",
   ["$mou__ganglie1"] = "一军之帅，岂惧暗箭之伤。",
   ["$mou__ganglie2"] = "宁陨沙场，不容折侮。",
 }
